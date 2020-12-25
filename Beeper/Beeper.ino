b4549b37169c9bda8e0d24b0d787f43228c53e5a
jasonjiangstuy <jasonjiangny@gmail.com>
2020-12-25 06:38:49 -0500

    debugged some issues that happended when i mounted the project

--- Beeper/Beeper.ino
+++ Beeper/Beeper.ino
@@ -1,17 +1,17 @@
-
- 
-#define SWITCH 0
+#define ZERO 0
 #define LED 1
 #define SOUND 2
+#define interval 500;
+unsigned int countdown = 0;
 
- long previousMillis = 0;
- long interval = 500;
+
 // the setup routine runs once when you press reset:
 void setup() {
+
   // initialize the LED pin as an output.
   pinMode(LED, OUTPUT);
-  // initialize the SWITCH pin as an input with resister
-  pinMode(SWITCH, INPUT);//_PULLUP);
+  // initialize the 2th pin as an input with resister
+  pinMode(ZERO, INPUT_PULLUP);
   // initialize the SOUND pin as an output.
   pinMode(SOUND, OUTPUT);
 
@@ -19,16 +19,17 @@ void setup() {
  
 // the loop routine runs over and over again forever:
 void loop() {
-  unsigned long currentMillis = millis();
-  if (digitalRead(SWITCH)) {  // if there is connectivity from 3v to pin 0
-    previousMillis = currentMillis;  //set up the timer that the beep is going to go off
-    
+  if (!digitalRead(ZERO)) {  // if there is connectivity from ground to pin 0
+    countdown = interval;  //set up the timer that the beep is going to go off
+   
   } 
-  if(currentMillis - previousMillis < interval) {
+  if(countdown > 0) {
+    countdown--;
     digitalWrite(LED, HIGH);    // light up the LED
-    analogWrite(SOUND, 255); 
-  }
-  else {
+    analogWrite(SOUND, 130); //255
+  } 
+  if (countdown <= 0){
+    countdown = 0;
     digitalWrite(LED, LOW);     // otherwise, turn it off
     analogWrite(SOUND, 0);
   }
