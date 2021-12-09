a268318965c6999f1c319f3b3edde863c2ef358d
jasonjiangstuy <jasonjiangny@gmail.com>
2021-12-09 01:05:51 -0500

    sync

--- Beeper/Beeper.ino
+++ Beeper/Beeper.ino
@@ -1,6 +1,3 @@
-#define ZERO 2
-#define LED 1
-#define SOUND 00
 #define interval 500;
 unsigned int countdown = 0;
 
@@ -8,30 +5,30 @@ unsigned int countdown = 0;
 // the setup routine runs once when you press reset:
 void setup() {
 
-  // initialize the LED pin as an output.
-  pinMode(LED, OUTPUT);
-  // initialize the 2th pin as an input with resister
-  pinMode(ZERO, INPUT_PULLUP);
-  // initialize the SOUND pin as an output.
-  pinMode(SOUND, OUTPUT);
+  // signaler
+  pinMode(0, OUTPUT);
+  // touch reciever
+  pinMode(1, INPUT_PULLUP);
+  // steal
+  pinMode(2, INPUT_PULLUP);
 
 }
  
 // the loop routine runs over and over again forever:
 void loop() {
-  if (!digitalRead(ZERO)) {  // if there is connectivity from ground to pin 0
+  if (!digitalRead(1)) {  // if there is connectivity from ground to pin 0
     countdown = interval;  //set up the timer that the beep is going to go off
    
   } 
   if(countdown > 0) {
     countdown--;
-    digitalWrite(LED, HIGH);    // light up the LED
-    analogWrite(SOUND, 255); //255
+    // digitalWrite(LED, HIGH);    // light up the LED
+    analogWrite(0, 255); //255
   } 
   if (countdown <= 0){
     countdown = 0;
-    digitalWrite(LED, LOW);     // otherwise, turn it off
-    analogWrite(SOUND, 0);
+    // digitalWrite(LED, LOW);     // otherwise, turn it off
+    analogWrite(0, 0);
   }
   delay(1); // run every ms// fastest touch is 2 ms on foil, this will catch all touches
 }
