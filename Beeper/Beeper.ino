0cf7c2a5e7e5577f87be8cf0a746febe114e1f0e
jasonjiangstuy <jasonjiangny@gmail.com>
2021-12-11 23:13:31 -0500

    mvp buzzer back

--- Beeper/Beeper.ino
+++ Beeper/Beeper.ino
@@ -1,4 +1,5 @@
-#define interval 500;
+#define interval 500
+#define LED 1
 unsigned int countdown = 0;
 
 // LED: pin 1: output: lights up when you get a valid touch, this signal gets
@@ -29,18 +30,18 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if (!digitalRead(1)) {  // if there is connectivity from ground to pin 0
+  if (!digitalRead(0)) {  // if there is connectivity from ground to pin 0
     countdown = interval;  //set up the timer that the beep is going to go off
   }
   if(countdown > 0) {
     countdown--;
     digitalWrite(LED, HIGH);    // light up the LED
-    analogWrite(0, 255); //255
+//    analogWrite(0, 255); //255
   }
   if (countdown <= 0){
     countdown = 0;
-    // digitalWrite(LED, LOW);     // otherwise, turn it off
-    analogWrite(0, 0);
+    digitalWrite(LED, LOW);     // otherwise, turn it off
+//    analogWrite(0, 0);
   }
   delay(1); // run every ms// fastest touch is 2 ms on foil, this will catch all touches
 }
